15c84d52f67fd4c4d489ccfcfd464968c341256d
kkulkarni2199 <kaumanoh@LIN59017969.corp.capgemini.com>
2021-11-16 14:04:25 +0530

    Fibonacci Series!

--- STSGithubProject/src/main/java/com/proj/STSGithubProject/Second.java
+++ STSGithubProject/src/main/java/com/proj/STSGithubProject/Second.java
@@ -1,5 +1,23 @@
-class Second{
-  public static void main(Str args[]){
+package com.proj.STSGithubProject;
+import java.util.*;
+
+public class Second{
+  public static void main(String args[]){
     System.out.println("This is file from second branch!!");
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter the count: ");
+    int count = scan.nextInt();
+    int n1=0,n2=1,n3,i;    
+    System.out.print(n1+" "+n2);//printing 0 and 1    
+       
+    for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+    {    
+     n3=n1+n2;    
+     System.out.print(" "+n3);    
+     n1=n2;    
+     n2=n3;    
+    }    
   }
 }
+
+
