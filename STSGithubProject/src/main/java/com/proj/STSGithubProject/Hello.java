87b864621e1731ce54ebf4104e8319e6d1ad5125
kkulkarni2199 <kaumanoh@LIN59017969.corp.capgemini.com>
2021-11-16 13:42:06 +0530

    Hello File!

---
+++ STSGithubProject/src/main/java/com/proj/STSGithubProject/Hello.java
@@ -0,0 +1,11 @@
+package com.proj.STSGithubProject;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello this is my first program!!");
+
+	}
+
+}
